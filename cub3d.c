0c01d2e96965a9b9f10132b33ab52165d27a1e9d
Hugo Villatte <huvillat@3C3.42angouleme.fr>
2025-03-24 10:48:23 +0100

    main et header

---
+++ cub3d.c
@@ -0,0 +1,51 @@
+#include "cub3d.h"
+
+int	ft_isitcub(char *str)
+{
+	int	i;
+	int j;
+	char cub[3] = "cub";
+
+	i = 0;
+	j = -1;
+	while(str[i])
+	{
+		if(str[i] == '.')
+		{
+			i++;
+			while(str[i] && str[i] == cub[++j])
+				++i;
+			if(j != 2)
+				return (1);
+		}
+		else
+			i++;
+	}
+	if(j == 2 && str[i - 4] == '.')
+		return (0);
+	else
+		return (1);
+}
+
+void	error_per(char *str, int flag)
+{
+	if(flag == 0)
+		perror(str);
+	exit(1);
+}
+
+void	error_msg(char *str, int flag)
+{
+	if(flag == 0)
+		ft_putendl(str, 2);
+	exit(1);
+}
+
+int	main(int ac, char **av)
+{
+	if(ac != 2)
+		return (1);	//error_msg();
+	if(ft_isitcub(av[1]) == 1)
+		return (1); //error_msg();
+	ft_cub3d(av[1]); //fonction principale
+}
---
+++ cub3d.h
@@ -0,0 +1,14 @@
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <fcntl.h>
+# include <X11/X.h>
+# include <X11/keysym.h>
+//# include "../mlx_linux/mlx.h"
+//# include "../libft/libft.h"
+
+#endif
