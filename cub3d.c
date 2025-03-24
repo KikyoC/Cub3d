e374689a2f2dfc52f1d229437255a42d6f923876
Hugo Villatte <huvillat@3C4.42angouleme.fr>
2025-03-24 13:19:56 +0100

    ajout de keybind & close_game & ++

--- cub3d.c
+++ cub3d.c
@@ -27,11 +27,41 @@ int	ft_isitcub(char *str)
 		return (1);
 }
 
+int	flood_fill(char **tmp, int x, int y, char c)
+{
+	int	up;
+	int	down;
+	int	right;
+	int	left;
+
+	if (tmp[x][y] == 'X' || tmp[x][y] == '1')
+		return (0);
+	if (tmp[x][y] != '1')
+		tmp[x][y] = 'X';
+	right = flood_fill(tmp, x, y + 1, c);
+	up = flood_fill(tmp, x - 1, y, c);
+	down = flood_fill(tmp, x + 1, y, c);
+	left = flood_fill(tmp, x, y - 1, c);
+	if (right == 1 || up == 1 || down == 1 || left == 1)
+		return (1);
+	if (tmp[x][y] == c)
+		return (1);
+	return (0);
+}
+
+void	ft_cub3d(char *str)
+{
+	// mlx_hook(game.win_ptr, KeyPress, KeyPressMask, ft_keybind, &game);
+	// mlx_hook(game.win_ptr, Expose, ExposureMask, ft_render_map, &game);
+	// mlx_hook(game.win_ptr, DestroyNotify, StructureNotifyMask, ft_close_game, &game);
+	// mlx_loop(game.mlx_ptr);
+}
+
 int	main(int ac, char **av)
 {
 	if(ac != 2)
 		return (1);	//error_msg();
 	if(ft_isitcub(av[1]) == 1)
 		return (1); //error_msg();
-	ft_cub3d(av[1]); //fonction principale
+	ft_cub3d(av[1]);
 }
--- error.c
+++ error.c
@@ -1,3 +1,9 @@
+#include "cub3d.h"
+
+/*If any misconfiguration of any kind is encountered in the file, the program
+must exit properly and return "Error\n" followed by an explicit error message
+of your choice*/
+
 void	error_per(char *str, int flag)
 {
 	if(flag == 0)
