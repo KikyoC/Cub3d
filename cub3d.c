4ca5f1f44788926a53d0b9f9cebf21c52733c991
Hugo Villatte <huvillat@3C3.42angouleme.fr>
2025-03-24 11:01:47 +0100

    ajout du error.c

--- cub3d.c
+++ cub3d.c
@@ -27,20 +27,6 @@ int	ft_isitcub(char *str)
 		return (1);
 }
 
-void	error_per(char *str, int flag)
-{
-	if(flag == 0)
-		perror(str);
-	exit(1);
-}
-
-void	error_msg(char *str, int flag)
-{
-	if(flag == 0)
-		ft_putendl(str, 2);
-	exit(1);
-}
-
 int	main(int ac, char **av)
 {
 	if(ac != 2)
