a4e3b32b7a2abd35e6301776c642b90ba231968f
togauthi <togauthier23@gmail.com>
2025-03-27 16:06:08 +0100

    Fixed some leaks

--- utils/destroyer.c
+++ utils/destroyer.c
@@ -4,13 +4,18 @@ int	ft_closegame(t_game *game)
 {
 	//mlx_destroy_image(game->mlx_ptr, game->wall.xpm_ptr); | 4 image au total
 	//mlx_destroy_image(game->mlx_ptr, game->ceiling.mlx_img);
-	mlx_destroy_image(game->mlx_ptr, game->win_tex.img_ptr);
-	mlx_destroy_image(game->mlx_ptr, game->win_g.img_ptr);
-	mlx_destroy_image(game->mlx_ptr, game->win_c.img_ptr);
-	mlx_destroy_window(game->mlx_ptr, game->win_ptr);
+	if (game->win_tex.img_ptr)
+		mlx_destroy_image(game->mlx_ptr, game->win_tex.img_ptr);
+	if (game->win_g.img_ptr)
+		mlx_destroy_image(game->mlx_ptr, game->win_g.img_ptr);	
+	if (game->win_c.img_ptr)
+		mlx_destroy_image(game->mlx_ptr, game->win_c.img_ptr);
+	if (game->win_ptr)
+		mlx_destroy_window(game->mlx_ptr, game->win_ptr);
 	mlx_destroy_display(game->mlx_ptr);
 	free(game->mlx_ptr);
-	exit(0);
+	// exit(0);
+	return (0);
 }
 
 void	destroy_row(t_point **row)
@@ -62,7 +67,9 @@ void	end_file(int config)
 int	destroy(t_game *game, int to_return)
 {
 	if (!game)
+	{
 		return (to_return);
+	}
 	end_file(game->config);
 	if (game->config > 2)
 		close(game->config);
@@ -80,11 +87,13 @@ int	destroy(t_game *game, int to_return)
 		free(game->images->ground);
 	if (game->images)
 		free(game->images);
+	printf("WOW\n");
 	if (game->mlx_ptr)
 		ft_closegame(game);
 	if (game->player)
 		free(game->player);
 	destroy_map(game);
 	free(game);
+	printf("Sucessfully destroyed everything\n");
 	return (to_return);
 }
