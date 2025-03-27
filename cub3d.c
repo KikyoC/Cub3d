0acc96a45f2ef9ee6587f24ed2e22247b8dd4a20
togauthi <togauthier23@gmail.com>
2025-03-27 16:06:35 +0100

    Removed some comments

--- cub3d.c
+++ cub3d.c
@@ -49,8 +49,8 @@ int	main(int ac, char **av)
 
 	if(ac != 2)
 		return (1);	//error_msg();
-	//if(ft_isitcub(av[1]) == 1)
-		//return (2); //error_msg();
+	if(ft_isitcub(av[1]) == 1)
+		return (2); //error_msg();
 	rows = get_row_size(av[1]);
 	if (rows < 0)
 	{
@@ -66,30 +66,6 @@ int	main(int ac, char **av)
 		return (destroy(game, 1));
 	printf("Sucessfully parsed the config\n");
 	//error_msg
-//	ft_cub3d(av[1]);
+	//ft_cub3d(av[1]);
 	return (destroy(game, 0));
 }
-
-//https://harm-smits.github.io/42docs/projects/cub3d
-
-// int	flood_fill(char **tmp, int x, int y, char c)
-// {
-// 	int	up;
-// 	int	down;
-// 	int	right;
-// 	int	left;
-
-// 	if (tmp[x][y] == 'X' || tmp[x][y] == '1')
-// 		return (0);
-// 	if (tmp[x][y] != '1')
-// 		tmp[x][y] = 'X';
-// 	right = flood_fill(tmp, x, y + 1, c);
-// 	up = flood_fill(tmp, x - 1, y, c);
-// 	down = flood_fill(tmp, x + 1, y, c);
-// 	left = flood_fill(tmp, x, y - 1, c);
-// 	if (right == 1 || up == 1 || down == 1 || left == 1)
-// 		return (1);
-// 	if (tmp[x][y] == c)
-// 		return (1);
-// 	return (0);
-// }
