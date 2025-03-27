ea19fbd051d5569784ee933655fe9c7a79a46aae
Hhugaur <hugovillatte.24@gmail.com>
2025-03-27 10:27:03 +0100

    +

--- exec/render_map.c
+++ exec/render_map.c
@@ -2,6 +2,8 @@
 
 int	ft_rendermap(t_game *game)
 {
-
+	check_move(game);
+	render_raycast(game);
+	mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, game->win_tex.img_ptr, 0, 0);
+	return (0);
 }
-
