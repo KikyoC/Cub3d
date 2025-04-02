16bab52db5f351765494cd162948b915710d2aff
togauthi <togauthier23@gmail.com>
2025-04-02 10:59:39 +0200

    update

--- exec/keybind.c
+++ exec/keybind.c
@@ -1,5 +1,21 @@
 #include "../cub3d.h"
 
+int	is_in_wall(float x, float y, float ray_angle, t_point ***map)
+{
+	int	a_x;
+	int a_y;
+
+	a_x = x;
+	a_y = y;
+	if (ray_angle < 3)
+	{
+		a_x += 1;
+		a_y += 1;
+	}
+
+	return (map[a_y][a_x]->c == '1' || map[a_y][a_x]->c == '\0');
+}
+
 void	move_player(int keysym, t_game *game)
 {
 	float	angle;
@@ -14,12 +30,16 @@ void	move_player(int keysym, t_game *game)
 		angle = game->ray.angle - 90;
 	else
 		angle = game->ray.angle;
+	while (angle > 360)
+		angle -= 360;
+	while (angle < 0)
+		angle += 360;
 	angle = angle * (M_PI / 180.0);
 	ray_cos = cos(angle);
 	ray_sin = sin(angle);
-	game->player->x += ray_cos * 0.1;
-	game->player->y += ray_sin * 0.1;
-	printf("Ray_angle = %f. Player position: x = %f ray_cos = %f, y = %f ray_sin = %f\n", angle, game->player->x, ray_cos, game->player->y, ray_cos);
+		game->player->x += ray_cos * 0.1;
+		game->player->y += ray_sin * 0.1;
+	//printf("Ray_angle = %f. Player position: x = %f ray_cos = %f, y = %f ray_sin = %f\n", angle, game->player->x, ray_cos, game->player->y, ray_cos);
 }
 
 void	check_move(t_game *game)
@@ -28,6 +48,10 @@ void	check_move(t_game *game)
 		game->ray.angle -= 3;
 	if (game->player->r_move)
 		game->ray.angle += 3;
+	while (game->ray.angle > 360)
+		game->ray.angle -= 360;
+	while (game->ray.angle < 0)
+		game->ray.angle += 360;
 	if (game->player->w_move)
 		move_player(KEY_W, game);
 	if (game->player->a_move)
--- utils/access.c
+++ utils/access.c
@@ -22,17 +22,11 @@ int max_y(t_point ***map)
 
 int	can_access(int x, int y, t_point ***map)
 {
-	//printf("Trying to see %i %i\n", x, y);
+	if (x < 0 || y < 0)
+		return (0);
 	if (max_y(map) <= y)
-	{
-	//	printf("Nope\n");
 		return (0);
-	}
 	if (max_x(map[y]) <= x)
-	{
-	//	printf("Nope\n");
 		return (0);
-	}
-	//printf("Uwu\n");
 	return (1);
 }
