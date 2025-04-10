f7434d41c57870803dd2d60a6f6002e61a0a6a67
togauthi <togauthier23@gmail.com>
2025-04-10 10:50:30 +0200

    Support dynamic place

--- parsing/player.c
+++ parsing/player.c
@@ -1,7 +1,11 @@
 #include "../cub3d.h"
 
-t_point	*get_player_pos(t_point ***map, double *x, double *y)
+t_point *get_player_pos(t_point ***map, double *x, double *y)
 {
+	t_point	*res;
+	int		found;
+
+	found = 0;
 	*y = 0.5;
 	while (map[(int)*y])
 	{
@@ -12,12 +16,20 @@ t_point	*get_player_pos(t_point ***map, double *x, double *y)
 				map[(int)*y][(int)*x]->c == 'S' \
 				|| map[(int)*y][(int)*x]->c == 'E' || \
 				map[(int)*y][(int)*x]->c == 'W')
-				return (map[(int)*y][(int)*x]);
+				found = 1;
+			if (found)
+				break ;
 			*x += 1;
 		}
+		if (found)
+			break ;
 		*y += 1;
 	}
-	return (NULL);
+	printf("Player pos is (%i,%i)\n", (int)*x, (int)*y);
+	res = map[(int)*y][(int)*x];
+	*x *= 64;
+	*y *= 64;
+	return (res);
 }
 
 int	setup_player(t_game *game)
@@ -27,20 +39,17 @@ int	setup_player(t_game *game)
 	game->player = ft_calloc(1, sizeof(t_player));
 	if (!game->player)
 		return (1);
-	game->player->point = get_player_pos(game->map, \
+	game->player->point = get_player_pos(game->map,
 		&game->player->x, &game->player->y);
 	if (!game->player->point)
 		return (1);
 	if (game->player->point->c == 'N')
-		game->player->direction = 270;
+		game->player->direction = 270 * M_PI / 180;
 	else if (game->player->point->c == 'E')
 		game->player->direction = 0;
 	else if (game->player->point->c == 'S')
-		game->player->direction = 90;
+		game->player->direction = 90 * M_PI / 180;
 	else if (game->player->point->c == 'W')
-		game->player->direction = 180;
-	game->player->direction = M_PI / 2;
-	game->player->x = 640;
-	game->player->y = 516;
+		game->player->direction = 180 * M_PI / 180;
 	return (0);
 }
