e9c49d416f775a4f478f1d8fa47420cae7cb7216
Hhugaur <hugovillatte.24@gmail.com>
2025-04-15 15:21:56 +0200

    normeandmore

--- utils/get_color.c
+++ utils/get_color.c
@@ -8,21 +8,13 @@ int	get_pixel_color(t_img *img, int x, int y)
 	return (*(int *)dst);
 }
 
-int get_good_pixel(t_img *img, double x, double z, int wall_height)
-{    
-    if (wall_height > 0)
-        z = z / wall_height * 64;
-    else
-        z = 0;
-    return (get_pixel_color(img, (int)(x * 64) % 64, (int)z % 64));
-}
-
-static int	mlx_pixel_get(t_img *data, int x, int y)
+int	get_good_pixel(t_img *img, double x, double z, int wall_height)
 {
-	char	*dst;
-
-	dst = data->addr + (y * data->line_len + x * (data->bpp / 8));
-	return (*(int *)dst);
+	if (wall_height > 0)
+		z = z / wall_height * 64;
+	else
+		z = 0;
+	return (get_pixel_color(img, (int)(x * 64) % 64, (int)z % 64));
 }
 
 int	get_good_pixell(t_game *game, t_img *img, t_ray ray, int z)
@@ -47,20 +39,20 @@ t_img	*get_good_mg(t_ray ray, t_game *game)
 	return (game->images->no);
 }
 
-t_img    *get_good_img(t_ray ray, t_game *game)
+t_img	*get_good_img(t_ray ray, t_game *game)
 {
-    if (ray.side == 0) // Vertical wall hit
-    {
-        if (ray.step_x > 0)
-            return (game->images->ea);
-        else
-            return (game->images->we);
-    }
-    else // Horizontal wall hit
-    {
-        if (ray.step_y > 0)
-            return (game->images->so);
-        else
-            return (game->images->no);
-    }
+	if (ray.side == 0)
+	{
+		if (ray.step_x > 0)
+			return (game->images->ea);
+		else
+			return (game->images->we);
+	}
+	else
+	{
+		if (ray.step_y > 0)
+			return (game->images->so);
+		else
+			return (game->images->no);
+	}
 }
